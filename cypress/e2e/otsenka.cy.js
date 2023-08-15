61ffd99dd305fb8fd82ff6e7f514268b9cc33931
rtvn-alex <lord-ratven@rambler.ru>
2023-08-15 20:20:05 +0300

    several tests added to otsenka.cy.js

--- cypress/e2e/otsenka.cy.js
+++ cypress/e2e/otsenka.cy.js
@@ -5,7 +5,8 @@ import {
     navigate,
     auth,
     clickAnElement,
-    enterGradient
+    enterGradient,
+    parseToJSON
 } from "../../page-objects/functions.js"
 
 
@@ -15,6 +16,7 @@ describe('actions', () => {
         auth()
         enterGradient()
         clickAnElement('Оценка')
+        cy.intercept('https://dev-gradient.luxmsbi.com/api/v3/koob/data?elPotencial').as('elPotencial')
     })
 
 
@@ -26,5 +28,64 @@ describe('actions', () => {
         cy.contains('руб./тн').should('exist')                                                                  //Установлен размерность 'руб./тн'
     })
 
+
+    it.skip('should check "plan+fact" switchers', () => {
+        // НЕ РАБОТАЕТ - либо починить, либо удалить
+        cy.wait('@elPotencial')
+        .then(cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
+        .then(cy.wait('@elPotencial')
+        .then((xhr) => {
+                    expect(xhr.request.body.filters.mnt_period[1]).to.be.eq("9+3")
+                    expect(parseToJSON(xhr)[0]).to.have.property('mnt_period', "9+3")
+        })))
+    })
+
+
+    it.skip('should correctly check "plan+fact" switchers', () => {
+        // НЕ РАБОТАЕТ - либо починить, либо удалить
+
+        //cy.wait('@elPotencial', {timeout:10000})
+        //cy.wait('@elPotencial', {timeout:10000})
+        cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
+        cy.wait(5000)
+        cy.wait('@elPotencial', {timeout:10000}).then((xhr) => {
+            expect(xhr.request.body.filters.mnt_period[1]).to.be.eq("9+3")
+            expect(parseToJSON(xhr)[0]).to.have.property('mnt_period', "9+3")
+        })
+    })
+
+
+    it('should check changing of names after switching "plan+fact"', () => {
+        cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
+        cy.get('button.PeriodsSelector__Button.active').should('have.text', '9+3')
+        cy.get('.GradientVizel__Potencial_Title').should('contain.text', '9+3')
+        cy.get('.GradientVizel__Title').should('contain.text', '9+3')
+    })
+
+
+    it('should check changing of names after choosing a period from the list', () => {
+        cy.get('button.last').click()
+        cy.get('li.MainPane__PeriodsSelectItem:nth-of-type(5):last').click() 
+        cy.get('button.PeriodsSelector__Button.active').should('not.exist')
+        cy.get('.GradientVizel__Potencial_Title').should('contain.text', '4+8')
+        cy.get('.GradientVizel__Title').should('contain.text', '4+8')
+    })
+
+
+    it('should change the year', () => {
+        cy.get('div.CustomDatePickerTrigger').click()
+        clickAnElement('2021')
+        cy.get('.GradientVizel__Potencial_Title').should('contain.text', '2021')
+        cy.get('.GradientVizel__Title').should('contain.text', '2021')
+        cy.get('div.react-datepicker__year--container').should('not.exist')
+    })
+
+
+    it.only('should delete an active correctly', () => {
+        cy.contains('div.GBarChart__XAxisTitle', 'Хантос').should('be.visible')
+        cy.contains('span', 'Хантос').siblings('i').click()
+       // cy.wait(3000)
+        cy.contains('Хантос').should('not.be.visible')
+    })
 
 })
