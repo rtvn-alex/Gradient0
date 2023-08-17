76d644692e4d8538cfd292ae82428d99fea94647
rtvn-alex <lord-ratven@rambler.ru>
2023-08-17 19:44:16 +0300

    Some deleting and adding tests added to 'otsenka' file

--- cypress/e2e/otsenka.cy.js
+++ cypress/e2e/otsenka.cy.js
@@ -17,6 +17,8 @@ describe('actions', () => {
         enterGradient()
         clickAnElement('Оценка')
         cy.intercept('https://dev-gradient.luxmsbi.com/api/v3/koob/data?elPotencial').as('elPotencial')
+        //cy.intercept('https://dev-gradient.luxmsbi.com/api/v3/koob/data?elPotencial').as('elPotencial1')
+        //cy.intercept('https://dev-gradient.luxmsbi.com/api/v3/koob/data?elPotencial').as('elPotencial2')
     })
 
 
@@ -31,24 +33,31 @@ describe('actions', () => {
 
     it.skip('should check "plan+fact" switchers', () => {
         // НЕ РАБОТАЕТ - либо починить, либо удалить
-        cy.wait('@elPotencial')
-        .then(cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
+        //cy.wait('@elPotencial')
+        
+        cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
         .then(cy.wait('@elPotencial')
         .then((xhr) => {
-                    expect(xhr.request.body.filters.mnt_period[1]).to.be.eq("9+3")
-                    expect(parseToJSON(xhr)[0]).to.have.property('mnt_period', "9+3")
-        })))
+            expect(xhr.request.body.filters.mnt_period[1]).to.be.eq("9+3")
+            expect(parseToJSON(xhr)[0]).to.have.property('mnt_period', "9+3")
+        }))
     })
 
 
-    it.skip('should correctly check "plan+fact" switchers', () => {
+    it.skip('should correctly check "plan+fact" switchers', async () => {
         // НЕ РАБОТАЕТ - либо починить, либо удалить
+        // ASYNC и AWAIT мб тоже лишние
 
-        //cy.wait('@elPotencial', {timeout:10000})
-        //cy.wait('@elPotencial', {timeout:10000})
-        cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
-        cy.wait(5000)
-        cy.wait('@elPotencial', {timeout:10000}).then((xhr) => {
+        await cy.wait('@elPotencial', {timeout:10000})
+        await cy.wait('@elPotencial', {timeout:10000})
+        await cy.wait(5000)
+        //debugger
+        await cy.get('div.GradientVizel__Potencial_Chart_GBar_Value', {timeout:10000}).should('be.visible')
+        await cy.get('button.PeriodsSelector__Button:nth-of-type(4)').click()
+        await cy.wait(5000)
+        //debugger
+        await cy.wait('@elPotencial2')
+        .then((xhr) => {
             expect(xhr.request.body.filters.mnt_period[1]).to.be.eq("9+3")
             expect(parseToJSON(xhr)[0]).to.have.property('mnt_period', "9+3")
         })
@@ -81,11 +90,45 @@ describe('actions', () => {
     })
 
 
-    it.only('should delete an active correctly', () => {
-        cy.contains('div.GBarChart__XAxisTitle', 'Хантос').should('be.visible')
-        cy.contains('span', 'Хантос').siblings('i').click()
-       // cy.wait(3000)
-        cy.contains('Хантос').should('not.be.visible')
+    it('should delete an active correctly', () => {
+        cy.get('div.DsShellMain').scrollTo('bottom')
+        cy.contains('div.GBarChart__XAxisTitle', Cypress.env('someAct')).should('exist')
+        cy.get('div.DsShellMain').scrollTo('top')
+        cy.contains('span', Cypress.env('someAct')).siblings('i').click()
+        cy.contains(Cypress.env('someAct')).should('not.be.visible')
     })
 
+
+    it('should check deleting of actives from the list', () => {
+        clickAnElement('Активы')
+        cy.get('div.ds_3.open').should('exist')
+        clickAnElement('Очистить все')
+        Cypress.env('activesWithoutYamal').forEach((act) => {
+            cy.contains('label.AppCheckbox', act).children('span.AppCheckbox__checkmark').should('not.be.checked')
+        })
+        clickAnElement('Применить')
+        cy.wait(1000)
+        cy.get('span.Tag:nth-of-type(2)').should('not.exist')
+        Cypress.env('activesWithoutYamal').forEach((act) => {
+            cy.contains(act).should('not.be.visible')
+        })
+    })
+
+
+    it.only('should check adding of actives to the list', () => {
+        
+        // РАЗОБРАТЬСЯ!!!
+
+        clickAnElement('Активы')
+        cy.contains('label.AppCheckbox', Cypress.env('someAct')).siblings('i').children('svg').click()
+        cy.contains('Зимнее').children('span').click()
+       // cy.log(cy.get('ul.AppTree__ChildList:nth-child(-n + 3)').map('innerText'))
+        /*for (let i of cy.get('ul.AppTree__ChildList:nth-of-child(-n + 3)').map('innerText')){
+            cy.get('i span').check()
+        }*/
+        for (let i of Cypress.env('digits').slice(0, 2)){
+            cy.get('ul.AppTree__ChildList :nth-child(' + `${i}` +') span').click()
+        }
+        cy.get('ul.AppTree__ChildList :nth-child(4) span').should('be.disabled')
+    })
 })
